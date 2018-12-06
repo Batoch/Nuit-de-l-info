c6a6cddf1a85d980ea282ef48ade370307e19221
NGUYEN Marc <marc.nguyen@etu.emse.fr>
2018-12-06 23:23:20 +0100

    Compatibility Webcam

---
+++ js/webcam.js
@@ -0,0 +1,7 @@
+let video = document.getElementById('videoElement');
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    });
+}
