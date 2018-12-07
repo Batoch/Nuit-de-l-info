649430ebf79185ed9f87f7e87c941727efb21588
NGUYEN Marc <marc.nguyen@etu.emse.fr>
2018-12-07 01:06:41 +0100

    Fix pls

--- js/webcam.js
+++ js/webcam.js
@@ -1,13 +1,13 @@
 
-var video = document.getElementById('video');
+var video = document.getElementById('videoElement');
 
 // Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   // Not adding `{ audio: true }` since we only want video now
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function(stream) {
-    //video.src = window.URL.createObjectURL(stream);
-    video.srcObject = stream;
+    video.src = window.URL.createObjectURL(stream);
+    //video.srcObject = stream;
     video.play();
   });
 }
